500c7cf9b63ac9543178d23940c8cf7883c21bf8
Amirali Amanipour <arshia.ala80@gmail.com>
2022-09-08 14:13:24 +0430

    adding images

---
+++ src/data.js
@@ -0,0 +1,56 @@
+
+import mb1 from './images/samsungmb1.jpg';
+import headphone1 from './images/headphone.jpg';
+import headphone2 from './images/headphone2.jpg';
+import headphone3 from './images/headphone3.jpg';
+
+
+
+const data=[
+
+    {
+        id:1,
+        brand:'razer',
+        image:headphone1,
+        name:'razer s21',
+        category:'headphone',
+        price:'1900000',
+        type:'normal',
+    }
+    
+    ,
+    {
+        id:2,
+        brand:'razer',
+        image:headphone2,
+        name:'razer ev72',
+        category:'headphone',
+        price:'1800000',
+        type:'normal',
+    }
+    ,
+    {
+        id:3,
+        brand:'logitech',
+        image:headphone3,
+        name:'logitech xy2.1',
+        category:'headphone',
+        price:'1700000',
+        type:'normal',
+    },
+
+    {
+        id:4,
+        brand:'samsung',
+        image:mb1,
+        name:'samsung galaxy21',
+        category:'mobile',
+        price:'2000000',
+        type:'discount',
+        priceDiscount:'1990000',
+    }
+]
+
+
+export {data}
+
