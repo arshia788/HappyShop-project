b5a8e78dd5ff59de0a34ff7056f71285e4f7db02
Amirali Amanipour <arshia.ala80@gmail.com>
2022-09-09 14:40:41 +0430

    adding TomFord ui

--- src/components/PerfumesSircle.js
+++ src/components/PerfumesSircle.js
@@ -1,9 +1,37 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core';
+import style from './perfumeSircle.module.css'
+
+
+const useStyles=makeStyles((theme)=>({
+    container:{
+        marginTop:theme.spacing(5.3)
+    },
+
+    holder:{
+        width:'90%',
+        margin:'auto',
+        border:'1px solid black',
+    }
+}))
 
 const PerfumesSircle = () => {
+
+    const classes= useStyles()
+
     return (
-        <div>
-            andas
+        <div className={classes.container}>
+
+            <div className={classes.holder}>
+                <div>
+                    <p>tomFords perfumes</p>
+                </div>
+
+                <div className={classes.holderCards}>
+
+                </div>
+            </div>
+
         </div>
     );
 };
